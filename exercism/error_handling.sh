88ea00ddff5fdc4279e3ac088f7465660ba4a27d
Jorge Corral <joc.palommares@gmail.com>
2022-08-16 22:24:29 +0000

    added error_handling.sh

--- exercism/error_handling.sh
+++ exercism/error_handling.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 #https://exercism.org/tracks/bash/exercises/error-handling/edit
 
+main(){
+    if [ $# -eq 1 ]
+    then
+        echo "Hello, ""$@"
+    else 
+        echo "Usage: error_handling.sh <person>"
+        exit 1
+    fi
+}
+
+main "$@"
